5422d755fac16bea1612e02660905ceaa233b0b3
Pawan <pshankar@endor.ai>
2024-12-21 11:03:38 -0800

    Add new person

--- app/tests/tests_utils.py
+++ app/tests/tests_utils.py
@@ -4,7 +4,7 @@ from app.utils import greet_user
 class TestUtils(unittest.TestCase):
     def test_greet_user(self):
         self.assertEqual(greet_user(), "Hello, Friend")  # Default value
-        self.assertEqual(greet_user("Alice"), "Hello, Alice!")
+        self.assertEqual(greet_user("Alice"), "Hello, New Person!")
 
 if __name__ == "__main__":
     unittest.main()
