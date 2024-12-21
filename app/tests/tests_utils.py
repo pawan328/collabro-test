39235ca2b63cab83e5be8c49f7b9251f7d6cf2de
Pawan <pshankar@endor.ai>
2024-12-20 16:46:03 -0800

    Add default value to greet_user function

--- app/tests/tests_utils.py
+++ app/tests/tests_utils.py
@@ -3,8 +3,8 @@ from app.utils import greet_user
 
 class TestUtils(unittest.TestCase):
     def test_greet_user(self):
+        self.assertEqual(greet_user(), "Hello, Friend")  # Default value
         self.assertEqual(greet_user("Alice"), "Hello, Alice!")
-        self.assertEqual(greet_user("World"), "Hello, World!")
 
 if __name__ == "__main__":
     unittest.main()
